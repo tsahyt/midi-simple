f96ad6b165d6271e26c4eef54a9b95c5f655abed
Paul Ogris <paul@tsahyt.com>
2017-02-13 21:35:22 +0100

    Add serialization for basic messages

---
+++ src/Sound/MIDI/Serialize.hs
@@ -0,0 +1,49 @@
+{-# LANGUAGE LambdaCase #-}
+module Sound.MIDI.Serialize where
+
+import Sound.MIDI.Types
+import Data.ByteString.Builder
+import Data.Monoid
+import Data.Word
+import Data.Bits
+
+message :: ChannelVoice -> Builder
+message = \case
+    NoteOff c p v -> 
+        channelStatus 0x80 c <> pitch p <> velocity v
+    NoteOn c p v -> 
+        channelStatus 0x90 c <> pitch p <> velocity v
+    Aftertouch c p t -> 
+        channelStatus 0xA0 c <> pitch p <> touch t
+    ControlChange c n d -> 
+        channelStatus 0xB0 c <> controller n <> word8 d
+    PatchChange c p -> 
+        channelStatus 0xC0 c <> patch p
+    ChannelPressure c t -> 
+        channelStatus 0xD0 c <> touch t
+    PitchBend c v -> 
+        channelStatus 0xE0 c <> pitchbend v
+
+channelStatus :: Word8 -> Channel -> Builder
+channelStatus p c = word8 $ p .|. getChannel c
+
+pitchbend :: Word16 -> Builder
+pitchbend v =
+    let l = fromIntegral $ v .&. 0x0007
+        m = fromIntegral $ v .&. 0x3f80
+     in word8 l <> word8 m
+
+pitch :: Pitch -> Builder
+pitch = word8 . getPitch
+
+patch :: Patch -> Builder
+patch = word8 . getPatch
+
+velocity :: Velocity -> Builder
+velocity = word8 . getVelocity
+
+touch :: Touch -> Builder
+touch = word8 . getTouch
+
+controller :: Controller -> Builder
+controller = word8 . getController
--- src/Sound/MIDI/Types.hs
+++ src/Sound/MIDI/Types.hs
@@ -23,15 +23,22 @@ data ChannelVoice
     | PatchChange Channel Patch
     | ChannelPressure Channel Touch
     | PitchBend Channel Word16
+    deriving (Eq, Show, Ord, Read)
 
 newtype Channel = Channel { getChannel :: Word8 }
+    deriving (Eq, Show, Ord, Read)
 
 newtype Pitch = Pitch { getPitch :: Word8 }
+    deriving (Eq, Show, Ord, Read)
 
 newtype Velocity = Velocity { getVelocity :: Word8 }
+    deriving (Eq, Show, Ord, Read)
 
 newtype Touch = Touch { getTouch :: Word8 }
+    deriving (Eq, Show, Ord, Read)
 
 newtype Controller = Controller { getController :: Word8 }
+    deriving (Eq, Show, Ord, Read)
 
 newtype Patch = Patch { getPatch :: Word8 }
+    deriving (Eq, Show, Ord, Read)
